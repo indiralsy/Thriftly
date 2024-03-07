d23445031134db975fb3946aa75c40116a19cfb4
indiralsy <129137210+indiralsy@users.noreply.github.com>
2024-03-07 13:04:53 +0700

    home.php

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- swipper css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+
+    <!-- font -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<!-- Mulai bagian header -->
+<section class="header">
+    <a href="home.php" class="logo"> Home</a>
+    
+    <nav class="navbar">
+        <a href = "home.php">home</a>
+        <a href = "about.php">about</a>
+        <a href = "package.php">package</a>
+        <a href = "book.php">book</a>
+
+    </nav>
+</section>
+
+<!-- swiper js link  -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
